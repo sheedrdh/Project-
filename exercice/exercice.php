35052c0f14b09376eea990d3867c9d30db3211cb
sheedrdh <sheedanshakileu@gmail.com>
2025-11-30 00:18:10 +0100

    idee trois avec une fonction

--- exercice/exercice.php
+++ exercice/exercice.php
@@ -13,44 +13,47 @@ if ($prixHT) {
 if ($Tva) {
     echo $Tva;
 }
-if ($prixTTC = $prixHT + ($prixHT * $Tva / 100));
+if ($prixTTC = $prixHT + ($prixHT * $Tva / 100))
+    ;
 
 echo $prixTTC
 
 
-// echo "ca c est le prixHT $prixHT" <"br">    .
+    // echo "ca c est le prixHT $prixHT" <"br">    .
 
-?>
+    ?>
 
-<h2> second  idee </h2>
- 
-    <?php
- 
-    echo $prixHT;
+<h2> second idee </h2>
+
+<?php
+
+echo $prixHT;
 
 
 echo $Tva;
 
-if ($prixTTC = $prixHT + ($prixHT * $Tva / 100));
+if ($prixTTC = $prixHT + ($prixHT * $Tva / 100))
+    ;
 
-    echo $prixTTC
+echo $prixTTC
     ?>
 
 
-<h2> 3 idee  </h2>
-    <?php
-
-    functon(formule){
- if ($prixTTC = $prixHT + ($prixHT * $Tva / 100));
-
-    }
+<h2> 3 idee </h2>
+<?php
 
 
 
+//    function "formule" (if($prixTTC = $prixHT + ($prixHT * $Tva / 100))) 
 
+function calculerPrixTTC($prixHT, $Tva)
+{
 
+    $prixTTC = $prixHT * (1 + $Tva / 100);
+    return $prixTTC;
+}
+echo "prix HT  $prixHT <br> ";
+echo "prix tva $Tva <br> ";
+echo "prix ttc $prixTTC <br> ";
 
-
-
-
-    ?>
+?>
