4018f609f76f3d128e2717b07ff9dcff91f7af66
sheedrdh <sheedanshakileu@gmail.com>
2025-12-02 02:06:59 +0100

    ajouter du text a rest de mais exo

--- exercice/exercice.php
+++ exercice/exercice.php
@@ -8,15 +8,15 @@ $Tva = 20;
 $prixTTC = null;
 
 if ($prixHT) {
-    echo $prixHT;
+    echo "prix HT  $prixHT <br> ";
 }
 if ($Tva) {
-    echo $Tva;
+    echo "prix tva $Tva <br> ";
 }
-if ($prixTTC = $prixHT + ($prixHT * $Tva / 100))
-    ;
+if ($prixTTC = $prixHT + ($prixHT * $Tva / 100));
 
-echo $prixTTC
+echo "prix ttc $prixTTC <br> "
+
 
 
     // echo "ca c est le prixHT $prixHT" <"br">    .
@@ -27,16 +27,19 @@ echo $prixTTC
 
 <?php
 
-echo $prixHT;
+if ($prixTTC = $prixHT + ($prixHT * $Tva / 100));
+
+    echo   "prix ttc $prixTTC <br> ";
 
 
-echo $Tva;
+      echo "prix HT  $prixHT <br> " ;
 
-if ($prixTTC = $prixHT + ($prixHT * $Tva / 100))
-    ;
+        echo $Tva;
 
-echo $prixTTC
+
     ?>
+
+
 
 
 <h2> 3 idee </h2>
